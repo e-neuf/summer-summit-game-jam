60e6e335127bd68d9452586735e15cea85ffbe23
Ellie Neufeld <ellie1neufeld@gmail.com>
2026-09-16 14:11:37 -0700

    simplify music switch and hopefully fixed positive music bug

--- scripts/level.gd
+++ scripts/level.gd
@@ -2,9 +2,6 @@ extends Node2D
 
 const MAIN_MENU = "res://main_menu.tscn"
 
-#var positive_song = preload("res://assets/music/Somewhere Sunny.mp3")
-#var negative_song = preload("res://assets/music/Private Reflection.mp3")
-
 var positive_song_progress = 0.0
 var negative_song_progress = 0.0
 
@@ -53,46 +50,29 @@ func on_polarity_flip(pol: int) -> void:
 	# Save previous song progress
 	if (pol < 0):
 		positive_song_progress = MusicHandler.pause()
-		#$AudioStreamPlayer.get_playback_position()
-		#var stream_length = $AudioStreamPlayer.stream.get_length() if $AudioStreamPlayer.stream else 0.0
-		#positive_song_progress = clamp(positive_song_progress, 0.0, stream_length)
-		#print("Positive song progress: %f | Total: %f" % [positive_song_progress, stream_length])
 	else:
 		negative_song_progress = MusicHandler.pause()
-		#$AudioStreamPlayer.get_playback_position()
-		#var stream_length = $AudioStreamPlayer.stream.get_length() if $AudioStreamPlayer.stream else 0.0
-		#negative_song_progress = clamp(negative_song_progress, 0.0, stream_length)
-		#print("Negative song progress: %f | Total: %f" % [negative_song_progress, stream_length])
-	MusicHandler.stop()
-	if (pol < 0 && negative_song_progress != 0):
-		print("neh music from prev progress")
-		MusicHandler.play_music_track(
-			MusicHandler.MUSIC_TRACKS.Polarity_Negative_Music,
-			negative_song_progress,
-		)
-	elif (pol < 0 && negative_song_progress == 0):
-		print("neg music from beg")
-		MusicHandler.play_music_track_from_beg(MusicHandler.MUSIC_TRACKS.Polarity_Negative_Music)
-	elif (pol > 0 && positive_song_progress != 0):
-		print("Pos from not beg")
-		MusicHandler.play_music_track(
-			MusicHandler.MUSIC_TRACKS.Polarity_Postive_Music,
-			positive_song_progress,
-		)
-	elif (pol > 0):
-		print("Pos from beg")
-		MusicHandler.play_music_track_from_beg(MusicHandler.MUSIC_TRACKS.Polarity_Postive_Music)
-		positive_song_progress = $AudioStreamPlayer.get_playback_position()
-		var stream_length = $AudioStreamPlayer.stream.get_length() if $AudioStreamPlayer.stream else 0.0
-		positive_song_progress = clamp(positive_song_progress, 0.0, stream_length)
-		#print("Positive song progress: %f | Total: %f" % [positive_song_progress, stream_length])
-	else:
-		negative_song_progress = $AudioStreamPlayer.get_playback_position()
-		var stream_length = $AudioStreamPlayer.stream.get_length() if $AudioStreamPlayer.stream else 0.0
-		negative_song_progress = clamp(negative_song_progress, 0.0, stream_length)
-		#print("Negative song progress: %f | Total: %f" % [negative_song_progress, stream_length])
 
-		#$AudioStreamPlayer.stop()
-	#$AudioStreamPlayer.stream = negative_song if pol < 0 else positive_song
-	#$AudioStreamPlayer.play(negative_song_progress if pol < 0 else positive_song_progress)
-	#
+	MusicHandler.stop()
+	if (pol < 0):
+		if (negative_song_progress != 0.0):
+			print("neg music from prev progress")
+			MusicHandler.play_music_track(
+				MusicHandler.MUSIC_TRACKS.Polarity_Negative_Music,
+				negative_song_progress,
+			)
+		else:
+			print("neg music from beg")
+			MusicHandler.play_music_track_from_beg(
+				MusicHandler.MUSIC_TRACKS.Polarity_Negative_Music
+			)
+	else:
+		if (positive_song_progress != 0.0):
+			print("Pos from not beg")
+			MusicHandler.play_music_track(
+				MusicHandler.MUSIC_TRACKS.Polarity_Postive_Music,
+				positive_song_progress,
+			)
+		else:
+			print("Pos from beg")
+			MusicHandler.play_music_track_from_beg(MusicHandler.MUSIC_TRACKS.Polarity_Postive_Music)
